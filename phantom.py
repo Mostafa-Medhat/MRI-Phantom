155fd8588f5b0b689c914f17bcf5ba30c4647755
omarmansour <omarmansour557@gmail.com>
2023-03-05 21:28:14 +0200

    initial files

---
+++ phantom.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import uic
+
+
+class Phantom(qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("/home/omar/Documents/Phantom-MRI/src/ui/Phantom.ui", self)
