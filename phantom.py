15fb018deb4d51a4b006550420146ed5da0508f5
Mostafa-Medhat <mmedhat72@gmail.com>
2023-03-10 01:38:17 +0200

    Add K-space and reconstruction functions

--- phantom.py
+++ phantom.py
@@ -17,6 +17,18 @@ from PyQt5.QtWidgets import QFileDialog, QSizePolicy
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
+from matplotlib import pyplot as plt
+import math as m
+# from skimage.color import rgb2gray
+# from skimage.io import imread
+import cv2
+import numpy as np
+from scipy import fftpack
+from scipy.spatial.transform import Rotation as R
+# import numpy as np
+from collections import Counter
+import cmath
+
 from matplotlib.figure import Figure
 from scipy.stats import norm
 
@@ -48,7 +60,7 @@ class Phantom(qtw.QWidget):
                                      self.axis_sequence_custom_PG, self.axis_sequence_custom_FG,
                                      self.axis_sequence_custom_RO]
 
-        self.pushButton_openSequence.clicked.connect(lambda: self.json_read())
+        self.pushButton_openSequence.clicked.connect(lambda: self.sequence_read())
         self.pushButton_apply.clicked.connect(lambda: self.custom_sequence())
         self.pushButton_clear.clicked.connect(lambda: self.clear_all())
         self.pushButton_openPhantom.clicked.connect(lambda: self.phantom_read())
@@ -88,7 +100,7 @@ class Phantom(qtw.QWidget):
 
         return canvas_sequence, axis_sequence_RF, axis_sequence_SS, axis_sequence_PG, axis_sequence_FG, axis_sequence_RO
 
-    def json_read(self):
+    def sequence_read(self):
         sequence_path = QFileDialog.getOpenFileName(
             self, "Open File", "sequence", filter="Json files (*.json)")[0]
         if sequence_path != "":
@@ -139,6 +151,11 @@ class Phantom(qtw.QWidget):
                      (dataFrame['RO'].Amp * SS_step), color=colors[4])
         axes[4].set_ylabel("RO")
 
+        # Setting Y limits of custom sequenec to be the same as loaded sequence
+        if axes is self.axes_sequence_custom:
+            for axis, axis_custom in zip(self.axes_sequence, self.axes_sequence_custom):
+                ymin, ymax = axis.get_ylim()
+                axis_custom.set_ylim(ymin, ymax)
         #
         canvas.draw()
 
@@ -149,6 +166,7 @@ class Phantom(qtw.QWidget):
             print(self.df_custom)
             self.df_custom['RF'].Amp = self.spinBox_RF.value()
             print(self.df_custom)
+
             self.plotting_sequence(self.axes_sequence_custom, self.canvas_sequence_custom, self.df_custom)
 
     def clear_all(self):
@@ -157,30 +175,6 @@ class Phantom(qtw.QWidget):
             axis_custom.clear()
         self.canvas_sequence.draw()
         self.canvas_sequence_custom.draw()
-
-    def phantom_read(self):
-        phantom_path = QFileDialog.getOpenFileName(self, "Open File", "src/docs/phantom images", filter="Images files ("
-                                                                                                        "*.jpg *.jpeg "
-                                                                                                        "*.png)")[0]
-        if phantom_path != "":
-            img = cv2.imread(phantom_path, cv2.IMREAD_GRAYSCALE)
-            w, h = int(self.figure_Orig_Spat.get_figwidth() * self.figure_Orig_Spat.dpi), int(
-                self.figure_Orig_Spat.get_figheight() * self.figure_Orig_Spat.dpi)
-            img = cv2.resize(img, (w, h), interpolation=cv2.INTER_AREA)
-            self.axis_Orig_Spat.imshow(img, cmap='gray')
-            self.canvas_Orig_Spat.draw()
-
-            # Compute the Fourier Transform of the image
-            f = np.fft.fft2(img)
-
-            # Shift the zero-frequency component to the center of the spectrum
-            fshift = np.fft.fftshift(f)
-
-            # Compute the magnitude spectrum of the Fourier Transform
-            magnitude_spectrum = 20 * np.log(np.abs(fshift))
-
-            self.axis_Orig_Fourier.imshow(magnitude_spectrum, cmap='gray')
-            self.canvas_Orig_Fourier.draw()
 
     def phantom_layout(self):
 
@@ -204,9 +198,7 @@ class Phantom(qtw.QWidget):
         self.axis_kspace = self.figure_kspace.add_subplot()
         self.canvas_kspace = FigureCanvas(self.figure_kspace)
         self.figure_kspace.subplots_adjust(left=0, right=1, bottom=0, top=1)
-        self.canvas_kspace.figure.set_facecolor('black')
         self.axis_kspace.set_facecolor('black')
-
         self.gridLayout_2.addWidget(self.canvas_kspace)
 
         self.figure_reconstruct = Figure(figsize=(20, 20), dpi=100)
@@ -221,3 +213,97 @@ class Phantom(qtw.QWidget):
         for axis in self.axes_phantom:  # removing axes from the figure
             axis.set_xticks([])
             axis.set_yticks([])
+
+    def phantom_read(self):
+        phantom_path = QFileDialog.getOpenFileName(self, "Open File", "src/docs/phantom images", filter="Images files ("
+                                                                                                        "*.jpg *.jpeg "
+                                                                                                        "*.png)")[0]
+        if phantom_path != "":
+            self.img = cv2.imread(phantom_path, cv2.IMREAD_GRAYSCALE)
+            w, h = int(self.figure_Orig_Spat.get_figwidth() * self.figure_Orig_Spat.dpi), int(
+                self.figure_Orig_Spat.get_figheight() * self.figure_Orig_Spat.dpi)
+            self.img = cv2.resize(self.img, (w, h), interpolation=cv2.INTER_AREA)
+            self.axis_Orig_Spat.imshow(self.img, cmap='gray')
+            self.canvas_Orig_Spat.draw()
+
+            # Compute the Fourier Transform of the image
+            f = np.fft.fft2(self.img)
+
+            # Shift the zero-frequency component to the center of the spectrum
+            fshift = np.fft.fftshift(f)
+
+            magnitude_spectrum = 20 * np.log(np.abs(fshift))
+
+            # Compute the magnitude spectrum of the Fourier Transform
+            self.axis_Orig_Fourier.imshow(magnitude_spectrum, cmap='gray')
+            self.canvas_Orig_Fourier.draw()
+
+            # Generate_kspace
+            self.generate_kspace()
+
+    def generate_kspace(self):
+
+        IMG = cv2.resize(self.img, (50, 50))
+
+        IMG_vector = np.zeros((IMG.shape[0], IMG.shape[1], 3), dtype=np.float_)
+        IMG_K_Space = np.zeros((IMG.shape[0], IMG.shape[1]), dtype=np.complex_)
+        X_Rotation = self.Rx(np.radians(90)) * self.Ry(0) * self.Rz(0)
+
+        for Krow in range(IMG.shape[0]):
+            print(Krow)
+            Gy_Phase = ((2 * np.pi) / IMG.shape[0]) * Krow
+            IMG_vector[:, :, :] = 0
+            # construct our vectors
+            for i in range(0, IMG.shape[0]):
+                for j in range(0, IMG.shape[1]):
+                    IMG_vector[i][j][2] = IMG[i][j]
+
+            # simulate RF
+            for i in range(0, IMG.shape[0]):
+                for j in range(0, IMG.shape[1]):
+                    IMG_vector[i][j] = IMG_vector[i][j] * X_Rotation
+
+            # simulate Gy
+            for i in range(0, IMG.shape[0]):
+                Z_Rotation = self.Rx(0) * self.Ry(0) * self.Rz((Gy_Phase / IMG.shape[0]) + ((Gy_Phase / IMG.shape[0]) * i))
+                for j in range(0, IMG.shape[1]):
+                    IMG_vector[i][j] = IMG_vector[i][j] * Z_Rotation
+
+                    # simulate Gx
+            for Kcol in range(0, IMG.shape[1]):
+                # stepi = 2*np.pi/(IMG.shape[0])*(Krow)
+                Gx_phase = 2 * np.pi / (IMG.shape[0]) * (Kcol)
+                for i in range(0, IMG.shape[1]):
+                    Z_Rotation = self.Rx(0) * self.Ry(0) * self.Rz((((2 * np.pi) / IMG.shape[0]) * i))
+                    for j in range(0, IMG.shape[0]):
+                        # theta=Gy_Phase*i + stepj*j
+                        IMG_vector[j][i] = IMG_vector[j][i] * Z_Rotation
+                        IMG_K_Space[Krow][Kcol] += (
+                                np.sqrt(np.square(IMG_vector[i][j][0]) + np.square(IMG_vector[i][j][1])) * np.exp(
+                            complex(0, -(Gy_Phase * i + Gx_phase * j))))
+
+        IMG_K_Space_shift = np.fft.fftshift(IMG_K_Space)
+
+        k_space_magnitude_spectrum = 20 * np.log(np.abs(IMG_K_Space_shift))
+
+        IMG_back = np.fft.ifft2(np.fft.ifftshift(IMG_K_Space_shift))
+
+        self.axis_kspace.imshow(k_space_magnitude_spectrum, cmap='gray')
+        self.canvas_kspace.draw()
+        self.axis_reconstruct.imshow(abs(IMG_back), cmap='gray')
+        self.canvas_reconstruct.draw()
+
+    def Rx(self, theta):
+        return np.matrix([[1, 0, 0],
+                          [0, m.cos(theta), -m.sin(theta)],
+                          [0, m.sin(theta), m.cos(theta)]])
+
+    def Ry(self, theta):
+        return np.matrix([[m.cos(theta), 0, m.sin(theta)],
+                          [0, 1, 0],
+                          [-m.sin(theta), 0, m.cos(theta)]])
+
+    def Rz(self, theta):
+        return np.matrix([[m.cos(theta), -m.sin(theta), 0],
+                          [m.sin(theta), m.cos(theta), 0],
+                          [0, 0, 1]])
