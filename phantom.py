74b351890d633a7fc57acf0fdd780998e923298c
Mostafa-Medhat <mmedhat72@gmail.com>
2023-03-09 17:52:18 +0200

    update ui

--- phantom.py
+++ phantom.py
@@ -25,22 +25,21 @@ class Phantom(qtw.QWidget):
         super().__init__()
 
         uic.loadUi("src/ui/Phantom.ui", self)
+        # self.splitter.setWidth(50, 1)
 
         self.df = None
         self.df_custom = None
 
         self.figure_sequence = Figure(dpi=80)
-        self.figure_sequence.set_facecolor('black')
         self.figure_sequence_custom = Figure(dpi=80)
-        self.figure_sequence_custom.set_facecolor('black')
 
         self.phantom_layout()
 
         self.canvas_sequence, self.axis_sequence_RF, self.axis_sequence_SS, self.axis_sequence_PG, self.axis_sequence_FG, self.axis_sequence_RO = self.sequence_layout(
-            self.figure_sequence, self.verticalLayout_4, "")
+            self.figure_sequence, self.verticalLayout_4)
 
         self.canvas_sequence_custom, self.axis_sequence_custom_RF, self.axis_sequence_custom_SS, self.axis_sequence_custom_PG, self.axis_sequence_custom_FG, self.axis_sequence_custom_RO = self.sequence_layout(
-            self.figure_sequence_custom, self.verticalLayout_6, "")
+            self.figure_sequence_custom, self.verticalLayout_6)
 
         self.axes_sequence = [self.axis_sequence_RF, self.axis_sequence_SS, self.axis_sequence_PG, self.axis_sequence_FG,
                               self.axis_sequence_RO]
@@ -55,50 +54,22 @@ class Phantom(qtw.QWidget):
 
         # self.sequence_custom_layout()
 
-    def sequence_layout(self, figure, layout, label):
+    def sequence_layout(self, figure, layout):
         ######################## Sequence Layout #########################
         axis_sequence_RF = figure.add_subplot(5, 1, 1)
-
-        axis_sequence_RF.set_ylabel("RF", color="white")
-        axis_sequence_RF.axes.set_title(str(label))
-
-        axis_sequence_RF.xaxis.label.set_color('white')
-        axis_sequence_RF.yaxis.label.set_color('white')
-        axis_sequence_RF.axes.tick_params(axis="x", colors="white")
-        axis_sequence_RF.axes.tick_params(axis="y", colors="white")
-        axis_sequence_RF.axes.title.set_color('white')
+        axis_sequence_RF.set_ylabel("RF")
 
         axis_sequence_SS = figure.add_subplot(5, 1, 2, sharex=axis_sequence_RF)
-        axis_sequence_SS.set_ylabel("SS", color="white")
-        axis_sequence_SS.xaxis.label.set_color('white')
-        axis_sequence_SS.yaxis.label.set_color('white')
-        axis_sequence_SS.axes.tick_params(axis="x", colors="white")
-        axis_sequence_SS.axes.tick_params(axis="y", colors="white")
-        axis_sequence_SS.axes.title.set_color('white')
+        axis_sequence_SS.set_ylabel("SS")
 
         axis_sequence_PG = figure.add_subplot(5, 1, 3, sharex=axis_sequence_RF)
-        axis_sequence_PG.set_ylabel("PG", color="white")
-        axis_sequence_PG.xaxis.label.set_color('white')
-        axis_sequence_PG.yaxis.label.set_color('white')
-        axis_sequence_PG.axes.tick_params(axis="x", colors="white")
-        axis_sequence_PG.axes.tick_params(axis="y", colors="white")
-        axis_sequence_PG.axes.title.set_color('white')
+        axis_sequence_PG.set_ylabel("PG")
 
         axis_sequence_FG = figure.add_subplot(5, 1, 4, sharex=axis_sequence_RF)
-        axis_sequence_FG.set_ylabel("FG", color="white")
-        axis_sequence_FG.xaxis.label.set_color('white')
-        axis_sequence_FG.yaxis.label.set_color('white')
-        axis_sequence_FG.axes.tick_params(axis="x", colors="white")
-        axis_sequence_FG.axes.tick_params(axis="y", colors="white")
-        axis_sequence_FG.axes.title.set_color('white')
+        axis_sequence_FG.set_ylabel("FG")
 
         axis_sequence_RO = figure.add_subplot(5, 1, 5, sharex=axis_sequence_RF)
-        axis_sequence_RO.set_ylabel("RO", color="white")
-        axis_sequence_RO.xaxis.label.set_color('white')
-        axis_sequence_RO.yaxis.label.set_color('white')
-        axis_sequence_RO.axes.tick_params(axis="x", colors="white")
-        axis_sequence_RO.axes.tick_params(axis="y", colors="white")
-        axis_sequence_RO.axes.title.set_color('white')
+        axis_sequence_RO.set_ylabel("RO")
         axis_sequence_RO.set_frame_on(False)
 
         canvas_sequence = FigureCanvas(figure)
@@ -109,10 +80,8 @@ class Phantom(qtw.QWidget):
 
         layout.addWidget(canvas_sequence)
 
-        axes_sequence = [axis_sequence_RF, axis_sequence_SS,
-                         axis_sequence_PG, axis_sequence_FG]
-
-        for axis in axes_sequence:  # removing axes from the figure
+        axes_sequence = [axis_sequence_RF, axis_sequence_SS, axis_sequence_PG, axis_sequence_FG]
+        for axis in axes_sequence:  ## removing axes from the figure
             axis.set_frame_on(False)
             axis.axes.get_xaxis().set_visible(False)
 
@@ -142,7 +111,7 @@ class Phantom(qtw.QWidget):
         axes[0].plot(RF_Duration + dataFrame['RF'].Duration / 2,
                      dataFrame['RF'].Amp * np.sinc(2 * RF_Duration),
                      color=colors[0])
-        axes[0].set_ylabel("RF", color="white")
+        axes[0].set_ylabel("RF")
 
         #
         SS_Duration = np.linspace(0, dataFrame['SS'].Duration, 100)
@@ -150,26 +119,26 @@ class Phantom(qtw.QWidget):
             np.sinc(RF_Duration) > dataFrame['RF'].Amp / 4
         axes[1].plot(SS_Duration, (dataFrame['SS'].Amp * SS_step),
                      color=colors[1])
-        axes[1].set_ylabel("SS", color="white")
+        axes[1].set_ylabel("SS")
 
         #
         PG_Duration = np.linspace(0, dataFrame['PG'].Duration, 100)
         for i in range(-dataFrame['PG'].Amp, dataFrame['PG'].Amp, 1):
             axes[2].plot(PG_Duration + dataFrame['PG'].Pos,
                          (i * SS_step), color=colors[2])
-        axes[2].set_ylabel("PG", color="white")
+        axes[2].set_ylabel("PG")
 
         #
         FG_Duration = np.linspace(0, dataFrame['FG'].Duration, 100)
         axes[3].plot(FG_Duration + dataFrame['FG'].Pos,
                      (dataFrame['FG'].Amp * SS_step), color=colors[3])
-        axes[3].set_ylabel("FG", color="white")
+        axes[3].set_ylabel("FG")
 
         #
         RO_Duration = np.linspace(0, dataFrame['RO'].Duration, 100)
         axes[4].plot(RO_Duration + dataFrame['RO'].Pos,
                      (dataFrame['RO'].Amp * SS_step), color=colors[4])
-        axes[4].set_ylabel("RO", color="white")
+        axes[4].set_ylabel("RO")
 
         #
         canvas.draw()
@@ -200,21 +169,27 @@ class Phantom(qtw.QWidget):
             pass
         else:
             img = cv2.imread(phantom_path)
-            img = cv2.resize(img, (558, 282), interpolation=cv2.INTER_AREA)
+            img = cv2.resize(img, (276, 253), interpolation=cv2.INTER_AREA)
             self.axes_Orig_Spat.imshow(img, cmap='gray')
             self.canvas_Orig_Spat.draw()
 
     def phantom_layout(self):
 
         ################ Phantom Original Layout #######################
-        self.figure_Orig_Spat = Figure(figsize=(20, 20), dpi=100)
 
+        self.figure_Orig_Fourier = Figure(figsize=(20, 20), dpi=100)
+        self.axes_Orig_Fourier = self.figure_Orig_Fourier.add_subplot()
+        self.canvas_Orig_Fourier = FigureCanvas(self.figure_Orig_Fourier)
+        self.canvas_Orig_Fourier.figure.set_facecolor("#19232D")
+        self.axes_Orig_Fourier.set_facecolor('black')
+        self.gridLayout.addWidget(self.canvas_Orig_Fourier)
+
+        self.figure_Orig_Spat = Figure(figsize=(20, 20), dpi=100)
         self.axes_Orig_Spat = self.figure_Orig_Spat.add_subplot()
         self.canvas_Orig_Spat = FigureCanvas(self.figure_Orig_Spat)
         self.canvas_Orig_Spat.figure.set_facecolor("#19232D")
         self.axes_Orig_Spat.set_facecolor('black')
-        self.verticalLayout_8.addWidget(self.canvas_Orig_Spat)
-        self.verticalLayout_8.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.addWidget(self.canvas_Orig_Spat)
 
         self.figure_kspace = Figure(figsize=(20, 20), dpi=100)
         self.axes_kspace = self.figure_kspace.add_subplot()
@@ -222,7 +197,7 @@ class Phantom(qtw.QWidget):
         self.canvas_kspace.figure.set_facecolor("#19232D")
         self.axes_kspace.set_facecolor('black')
 
-        self.horizontalLayout_3.addWidget(self.canvas_kspace)
+        self.gridLayout_2.addWidget(self.canvas_kspace)
 
         self.figure_reconstruct = Figure(figsize=(20, 20), dpi=100)
         self.axes_reconstruct = self.figure_reconstruct.add_subplot()
@@ -230,9 +205,9 @@ class Phantom(qtw.QWidget):
         self.canvas_reconstruct.figure.set_facecolor("#19232D")
         self.axes_reconstruct.set_facecolor('black')
 
-        self.horizontalLayout_3.addWidget(self.canvas_reconstruct)
+        self.gridLayout_2.addWidget(self.canvas_reconstruct)
 
-        self.axes_phantom = [self.axes_Orig_Spat,
+        self.axes_phantom = [self.axes_Orig_Spat, self.axes_Orig_Fourier,
                              self.axes_kspace, self.axes_reconstruct]
         for axis in self.axes_phantom:  # removing axes from the figure
             axis.set_xticks([])
